d6dddcca02a8a07ab31a2b66e9700bdb27f0ff1d
om <omjha003@gmail.com>
2024-10-15 18:01:28 +0530

    Implemented the read feature for the posts

--- index.js
+++ index.js
@@ -8,8 +8,29 @@ app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
 app.use(express.static(path.join(__dirname , "public")));
 
+let posts = [
+    {
+        username : "apnacollege",
+        content : "This is our own Dev channel"
+    },
+
+    {
+        username : "Om Kumar",
+        content : "I am selected for my google summer internship"
+    },
+
+    {
+        username : "Turkesh",
+        content : "I am big fan of om . He is very humble!"
+    }
+]
+
 app.get("/" , (req , res) => {
-    res.render("index.ejs");
+    res.send("We are in the root file")
+})
+
+app.get("/posts" , (req , res) => {
+    res.render("index.ejs" , {posts});
 })
 
 app.listen(port , (req , res) => {
