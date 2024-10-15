cb857222103d8dbf9b622c61247ab93d3baf1e77
om <omjha003@gmail.com>
2024-10-15 19:24:29 +0530

    Added lookup functionality for individual posts

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ app.use(express.urlencoded({extented : true}));
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
 app.use(express.static(path.join(__dirname , "public")));
+const { v4 : uuidv4 } = require("uuid");
+// uuidv4();
 
 let posts = [
-    {
+    {   
+        id : uuidv4(),
         username : "apnacollege",
         content : "This is our own Dev channel"
     },
 
     {
+        id : uuidv4(),
         username : "Om Kumar",
         content : "I am selected for my google summer internship"
     },
 
-    {
+    {   
+        id : uuidv4(),
         username : "Turkesh",
         content : "I am big fan of om . He is very humble!"
     }
@@ -37,9 +42,16 @@ app.get("/posts/new" , (req , res) => {
     res.render("new.ejs");
 })
 
+app.get("/posts/:id" , (req , res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("show.ejs" , {post});
+})
+
 app.post("/posts" , (req , res) => {
     let {username , content} = req.body;
-    posts.push({username , content});
+    let id = uuidv4();
+    posts.push({id , username , content});
     res.redirect("/posts"); // Yeh internally get method ke sath hi jayega /posts mai.
 })
 
